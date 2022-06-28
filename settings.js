44aa8e3e6d2af5431237e9c6f28bd20dbab023a4
psking12345 <108357489+psking12345@users.noreply.github.com>
2022-06-28 22:02:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,30 +20,30 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is PS KINGRED DRAGON🔥
+My Creator is 'pawan king ps😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94776436421'] //Owner number in aive msg
+global.premium = ['+94776436421'] //Owner Number info
+global.ownernomer = '+94776436421' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = '😎𝗔.𝗚.𝗣𝗮𝘄𝗮𝗻 𝗞𝗶𝗻𝗴' //Owner Name
+global.botname = ' ☬RED 𝗣𝗦 𝗞𝗜𝗡𝗚༒DRAGON ' //Bot Name
 
 global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
 global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬RED 𝗣𝗦 𝗞𝗜𝗡𝗚༒DRAGON࿐' //Bot Pacage Name
+global.author = '😎𝗔.𝗚.𝗣𝗔𝗪𝗔𝗡 𝗞𝗜𝗡𝗚❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
 
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
-global.footer = '©_Kumuthu.' // DO NOT CHANGE!
+global.footer = '©_𝗣𝗔𝗪𝗔𝗡 𝗞𝗜𝗡𝗚 𝗗𝗥𝗔𝗚𝗢𝗡.' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
     success: 'Done ✅',
@@ -53,7 +53,7 @@ global.mess = { // Error Mesages
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: '👉Pleas Wait,👈😋...',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
